8a5ddf1c19741aace63f63dc4a8c26a2fd3b1cf2
Vinayak <vinayak.kalmane@codecraft.co.in>
2024-08-08 11:53:10 +0530

    First Commit

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -7,7 +7,9 @@ export default {
   component: Button,
 } as Meta;
 
-const Template: StoryFn = (args) => <Button label="RESET" {...args} />;
+const Template: StoryFn = (args) => <Button onClick={function (): void {
+  throw new Error("Function not implemented.");
+} } disabled={false} label="RESET" {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
